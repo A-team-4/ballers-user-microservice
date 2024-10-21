539e7f45881e713febdc968fee3765f7b30263cd
obajide028 <obajide028@gmail.com>
2024-10-21 11:08:03 +0100

    fix: mongodb setup

--- src/config/dbConnect.ts
+++ src/config/dbConnect.ts
@@ -1,11 +1,8 @@
 import mongoose from 'mongoose';
-import { MONGO_URI } from './dbConfigSetup';
-
-// const dburl: string = process.env.MONGO_URI as string
 
 export const connectDB = async (): Promise<void> => {
   try {
-    await mongoose.connect(MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI!);
     console.log('MongoDb Connected');
   } catch (err) {
     console.error('Failed to connect to db', err);
